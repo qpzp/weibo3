ef7968be188e2bb6ff28a1347652672c269edd56
qpzp <qpzp@hotmail.com>
2019-11-28 12:56:11 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,20 @@
 <html>
 <head>
     <title>@yield('title','微博 App') - by ftw</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-@yield('content')
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">微博 App</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">help</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.default')
 @section('title','home')
 @section('content')
-    <h1>home</h1>
+    <div class="jumbotron">
+        <h1>hello 瓜子</h1>
+        <p class="lead">
+            你觉得瓜子好吃吗？
+        </p>
+        <p>我觉得还是很好吃的</p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
