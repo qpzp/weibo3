1ab0014ea82769183578cc2cf0b1c1e0ceda2792
qpzp <qpzp@hotmail.com>
2019-12-01 07:42:18 +0000

    增加csrf验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,22 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
-        return view('users.create');
-    }
+  public function create()
+  {
+    return view('users.create');
+  }
 
-    public function show(User $user)
-    {
-        return view('users.show', compact('user'));
-    }
+  public function show(User $user)
+  {
+    return view('users.show', compact('user'));
+  }
+
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'name' => 'required|max:50',
+      'email' => 'required|email|unique:users|max:255',
+      'password' => 'required|confirmed|min:6'
+    ]);
+  }
 }
